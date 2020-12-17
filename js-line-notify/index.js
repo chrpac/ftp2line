c0f2780bd3d17534cf5901da1bd92ea448052728
chrpac <chrpac@gmail.com>
2020-12-17 17:46:42 +0700

    edit path

--- js-line-notify/index.js
+++ js-line-notify/index.js
@@ -1,6 +1,6 @@
 const LineNotify = require("./src/client");
 hound = require('hound')
-watcher = hound.watch('img')
+watcher = hound.watch('/usr/src/app/img')
 
 const ACCESS_TOKEN = "8L5ACVF6JsdZPlQYgecJ3M7EhIwIrurFuo7XED1YqME";
 const notify = new LineNotify(`${ACCESS_TOKEN}`);
