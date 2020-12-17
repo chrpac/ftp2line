71f5ca82dc3446bfb4e9ebfbb9abfbe3cec1dc04
chrpac <chrpac@gmail.com>
2020-12-17 10:05:57 +0700

    first

---
+++ js-line-notify/index.js
@@ -0,0 +1,12 @@
+const LineNotify = require("./src/client");
+hound = require('hound')
+watcher = hound.watch('/usr/img')
+
+const ACCESS_TOKEN = "8L5ACVF6JsdZPlQYgecJ3M7EhIwIrurFuo7XED1YqME";
+const notify = new LineNotify(`${ACCESS_TOKEN}`);
+
+
+watcher.on('create', function(file, stats) {
+    notify.sendImage(file);
+    console.log(file + ' was created')
+  });
