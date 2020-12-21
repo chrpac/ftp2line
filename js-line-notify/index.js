3581eb9e2bf5fbea329ddfb812780df1b4e5e53d
chrpac <chrpac@gmail.com>
2020-12-21 17:51:52 +0700

    revise notify

--- js-line-notify/index.js
+++ js-line-notify/index.js
@@ -1,12 +1,41 @@
-const LineNotify = require("./src/client");
-hound = require('hound')
-watcher = hound.watch('/usr/src/app/img')
+const request = require('request')
+const fs = require('fs')
+//hound = require('hound')
+//watcher = hound.watch('/usr/src/app/img')
 
-const ACCESS_TOKEN = "8L5ACVF6JsdZPlQYgecJ3M7EhIwIrurFuo7XED1YqME";
-const notify = new LineNotify(`${ACCESS_TOKEN}`);
+token = '8L5ACVF6JsdZPlQYgecJ3M7EhIwIrurFuo7XED1YqME';
+message = 'HelloWorld';
+
+const lineNotify = (message, file) => {
+  request({
+    method: 'POST',
+    uri: 'https://notify-api.line.me/api/notify',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    auth: {
+      'bearer': token
+    },
+    formData: {
+      message,
+      imageFile : fs.createReadStream(file)
+    }
+  }, (err, httpResponse, body) => {
+    if(err){
+      console.log(err);
+    } else {
+      console.log({
+        httpResponse: httpResponse,
+        body: body
+      });
+    }
+  });
+}
 
 
 watcher.on('create', function(file, stats) {
-    notify.sendImage(file);
+    lineNotify('Hello World', file)
     console.log(file + ' was created')
   });
+
+//lineNotify('Hello Haha','default_image.png')
